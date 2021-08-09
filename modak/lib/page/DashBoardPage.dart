fed6e44bf6645a10cfe092019a776527e1bb8b72
Yun-SeYeong <dbstpdud09@naver.com>
2021-08-09 23:49:02 +0900

    [HOTFIX] fix filename

--- modak/lib/page/DashBoardPage.dart
+++ modak/lib/page/DashBoardPage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:modak/component/TodayCompingWidget.dart';
+import 'package:modak/component/TodayCampingWidget.dart';
 
 class DashBoardPage extends StatefulWidget {
   @override
