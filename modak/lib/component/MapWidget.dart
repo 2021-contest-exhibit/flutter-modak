25d072c88945539497042c01d4c385a9b4cff051
Yun-SeYeong <dbstpdud09@naver.com>
2021-09-12 00:26:58 +0900

    [ADD] google map component

---
+++ modak/lib/component/MapWidget.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Google Maps Demo',
+      home: MapWidget(),
+    );
+  }
+}
+
+
+class MapWidget extends StatefulWidget {
+  @override
+  MapWidgetState createState() => MapWidgetState();
+}
+
+class MapWidgetState extends State<MapWidget> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.39344116934309, 126.81330326976159),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.39344116934309, 126.81330326976159),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(
+      mapType: MapType.normal,
+      initialCameraPosition: _kGooglePlex,
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
+    );
+  }
+
+}
