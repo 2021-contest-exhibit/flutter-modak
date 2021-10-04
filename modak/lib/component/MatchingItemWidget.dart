c0b7a63f54ef06ba2d0f591c064b6fbad1f2b822
Yun-SeYeong <dbstpdud09@naver.com>
2021-10-04 18:37:38 +0900

    [UPDATE] update component

--- modak/lib/component/MatchingItemWidget.dart
+++ modak/lib/component/MatchingItemWidget.dart
@@ -20,7 +20,6 @@ class MatchingItemWidgetState extends State<MatchingItemWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.all(12.0),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
