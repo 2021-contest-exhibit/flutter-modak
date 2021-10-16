94d403a15337393997e2cb04bc67f5e498e6373f
sinkyoungdeok <tlsrudejr123@nate.com>
2021-10-16 22:03:21 +0900

    [ADD] (CampingSearchPage) 기본 UI세팅

--- modak/lib/page/CampingSearchPage.dart
+++ modak/lib/page/CampingSearchPage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
@@ -12,18 +11,59 @@ void main() {
   ));
 }
 
-
 class CampingSearchPage extends StatefulWidget {
   @override
   CampingSearchPageState createState() => CampingSearchPageState();
 }
 
-
 class CampingSearchPageState extends State<CampingSearchPage> {
   @override
   Widget build(BuildContext context) {
+    final double _width = MediaQuery.of(context).size.width;
+    final double _contentWidth = _width - 40;
     return Scaffold(
-
+      body: ListView(
+        physics: BouncingScrollPhysics(),
+        children: [
+          SizedBox(
+            height: 48.0,
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20.0),
+            width: _contentWidth,
+            height: 48,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.resolveWith((states) {
+                  return Colors.white;
+                }),
+                backgroundColor: MaterialStateProperty.resolveWith((states) {
+                  return Colors.white;
+                }),
+              ),
+              onPressed: () {},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/camping_search');
+                    },
+                    child: Text(
+                      "검색어를 입력해주세요",
+                      style: TextStyle(color: Colors.grey, fontSize: 20),
+                    ),
+                  ),
+                  Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
